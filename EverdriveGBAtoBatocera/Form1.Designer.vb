a3f22ddca9db1ac2dd07a0bf6c2131810d568780
soaresden <soaresden@gmail.com>
2021-07-06 21:16:04 +0200

    v1

--- EverdriveGBAtoBatocera/Form1.Designer.vb
+++ EverdriveGBAtoBatocera/Form1.Designer.vb
@@ -29,27 +29,35 @@ Partial Class Form1
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.EverdriveTab = New System.Windows.Forms.DataGridView()
         Me.BatoceraTab = New System.Windows.Forms.DataGridView()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.BatoceraPath = New System.Windows.Forms.TextBox()
         Me.RefreshBatocera = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GBAtoBato = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.CalculateDelta = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.ListBox2 = New System.Windows.Forms.ListBox()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupGBA = New System.Windows.Forms.GroupBox()
+        Me.GroupBatocera = New System.Windows.Forms.GroupBox()
+        Me.BatotoGBA = New System.Windows.Forms.Button()
         Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.EverdriveTab, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.BatoceraTab, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupGBA.SuspendLayout()
+        Me.GroupBatocera.SuspendLayout()
         Me.SuspendLayout()
         '
         'GBASD
         '
-        Me.GBASD.Location = New System.Drawing.Point(129, 182)
+        Me.GBASD.Location = New System.Drawing.Point(213, 65)
         Me.GBASD.Name = "GBASD"
         Me.GBASD.Size = New System.Drawing.Size(27, 20)
         Me.GBASD.TabIndex = 0
@@ -57,9 +65,9 @@ Partial Class Form1
         '
         'RefreshGBA
         '
-        Me.RefreshGBA.Location = New System.Drawing.Point(28, 213)
+        Me.RefreshGBA.Location = New System.Drawing.Point(167, 91)
         Me.RefreshGBA.Name = "RefreshGBA"
-        Me.RefreshGBA.Size = New System.Drawing.Size(111, 23)
+        Me.RefreshGBA.Size = New System.Drawing.Size(118, 35)
         Me.RefreshGBA.TabIndex = 1
         Me.RefreshGBA.Text = "Refresh Files"
         Me.RefreshGBA.UseVisualStyleBackColor = True
@@ -67,9 +75,9 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 254)
+        Me.PictureBox1.Location = New System.Drawing.Point(725, 35)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(144, 137)
+        Me.PictureBox1.Size = New System.Drawing.Size(108, 91)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
@@ -77,9 +85,9 @@ Partial Class Form1
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(12, 39)
+        Me.PictureBox2.Location = New System.Drawing.Point(17, 35)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(144, 137)
+        Me.PictureBox2.Size = New System.Drawing.Size(144, 91)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox2.TabIndex = 3
         Me.PictureBox2.TabStop = False
@@ -88,50 +96,41 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 7)
+        Me.Label1.Location = New System.Drawing.Point(213, 7)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(425, 25)
+        Me.Label1.Size = New System.Drawing.Size(457, 25)
         Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Everdrive GBA Saves to Batocera Sync"
+        Me.Label1.Text = "Everdrive GBA Saves to Batocera Sync v1"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(2, 185)
+        Me.Label2.Location = New System.Drawing.Point(164, 35)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(121, 13)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Drive Letter of your SD :" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(178, 35)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(67, 13)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Saves Files :"
-        '
         'EverdriveTab
         '
         Me.EverdriveTab.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.EverdriveTab.Location = New System.Drawing.Point(181, 52)
+        Me.EverdriveTab.Location = New System.Drawing.Point(6, 12)
         Me.EverdriveTab.Name = "EverdriveTab"
-        Me.EverdriveTab.Size = New System.Drawing.Size(392, 184)
+        Me.EverdriveTab.Size = New System.Drawing.Size(392, 154)
         Me.EverdriveTab.TabIndex = 9
         '
         'BatoceraTab
         '
         Me.BatoceraTab.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.BatoceraTab.Location = New System.Drawing.Point(181, 254)
+        Me.BatoceraTab.Location = New System.Drawing.Point(6, 15)
         Me.BatoceraTab.Name = "BatoceraTab"
-        Me.BatoceraTab.Size = New System.Drawing.Size(392, 184)
+        Me.BatoceraTab.Size = New System.Drawing.Size(392, 154)
         Me.BatoceraTab.TabIndex = 10
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(32, 394)
+        Me.Label4.Location = New System.Drawing.Point(612, 35)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(107, 13)
         Me.Label4.TabIndex = 13
@@ -139,89 +138,167 @@ Partial Class Form1
         '
         'BatoceraPath
         '
-        Me.BatoceraPath.Location = New System.Drawing.Point(12, 410)
+        Me.BatoceraPath.Location = New System.Drawing.Point(428, 51)
         Me.BatoceraPath.Name = "BatoceraPath"
-        Me.BatoceraPath.Size = New System.Drawing.Size(144, 20)
+        Me.BatoceraPath.Size = New System.Drawing.Size(291, 20)
         Me.BatoceraPath.TabIndex = 11
         Me.BatoceraPath.Text = "D:\Documents\a-Emulation et Jeux\Recalbox et Batocera\PI4Hybrid\batocera\"
         '
         'RefreshBatocera
         '
-        Me.RefreshBatocera.Location = New System.Drawing.Point(28, 436)
+        Me.RefreshBatocera.Location = New System.Drawing.Point(608, 91)
         Me.RefreshBatocera.Name = "RefreshBatocera"
-        Me.RefreshBatocera.Size = New System.Drawing.Size(111, 23)
+        Me.RefreshBatocera.Size = New System.Drawing.Size(111, 35)
         Me.RefreshBatocera.TabIndex = 14
         Me.RefreshBatocera.Text = "Refresh Files"
         Me.RefreshBatocera.UseVisualStyleBackColor = True
         '
-        'Button3
+        'GBAtoBato
         '
-        Me.Button3.Location = New System.Drawing.Point(619, 52)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(204, 23)
-        Me.Button3.TabIndex = 15
-        Me.Button3.Text = "Everdrive --> Batocera"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.GBAtoBato.Location = New System.Drawing.Point(98, 381)
+        Me.GBAtoBato.Name = "GBAtoBato"
+        Me.GBAtoBato.Size = New System.Drawing.Size(214, 30)
+        Me.GBAtoBato.TabIndex = 15
+        Me.GBAtoBato.Text = "Copy Everdrive Saves --> Batocera Saves"
+        Me.GBAtoBato.UseVisualStyleBackColor = True
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(619, 81)
+        Me.ListBox1.Location = New System.Drawing.Point(6, 189)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(204, 95)
+        Me.ListBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.ListBox1.Size = New System.Drawing.Size(184, 186)
         Me.ListBox1.TabIndex = 16
+        '
+        'CalculateDelta
+        '
+        Me.CalculateDelta.Location = New System.Drawing.Point(339, 91)
+        Me.CalculateDelta.Name = "CalculateDelta"
+        Me.CalculateDelta.Size = New System.Drawing.Size(164, 35)
+        Me.CalculateDelta.TabIndex = 21
+        Me.CalculateDelta.Text = "Filling Listboxes with Saves"
+        Me.CalculateDelta.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 171)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "FROM"
         '
         'ListBox2
         '
         Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(619, 213)
+        Me.ListBox2.Location = New System.Drawing.Point(214, 189)
         Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(204, 95)
-        Me.ListBox2.TabIndex = 17
+        Me.ListBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ListBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.ListBox2.Size = New System.Drawing.Size(184, 186)
+        Me.ListBox2.TabIndex = 23
         '
-        'Button4
+        'Label6
         '
-        Me.Button4.Location = New System.Drawing.Point(619, 186)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(204, 23)
-        Me.Button4.TabIndex = 18
-        Me.Button4.Text = "Batocera --> Everdrive"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(211, 171)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(22, 13)
+        Me.Label6.TabIndex = 24
+        Me.Label6.Text = "TO"
         '
-        'Button5
+        'GroupGBA
         '
-        Me.Button5.Location = New System.Drawing.Point(619, 314)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(204, 23)
-        Me.Button5.TabIndex = 19
-        Me.Button5.Text = "Full Sync (Most Up to Date File)"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.GroupGBA.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.GroupGBA.Controls.Add(Me.EverdriveTab)
+        Me.GroupGBA.Controls.Add(Me.GBAtoBato)
+        Me.GroupGBA.Controls.Add(Me.ListBox1)
+        Me.GroupGBA.Controls.Add(Me.Label5)
+        Me.GroupGBA.Controls.Add(Me.ListBox2)
+        Me.GroupGBA.Controls.Add(Me.Label6)
+        Me.GroupGBA.Location = New System.Drawing.Point(17, 132)
+        Me.GroupGBA.Name = "GroupGBA"
+        Me.GroupGBA.Size = New System.Drawing.Size(405, 417)
+        Me.GroupGBA.TabIndex = 34
+        Me.GroupGBA.TabStop = False
+        Me.GroupGBA.Text = "Everdrive to Batocera"
+        '
+        'GroupBatocera
+        '
+        Me.GroupBatocera.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.GroupBatocera.Controls.Add(Me.BatotoGBA)
+        Me.GroupBatocera.Controls.Add(Me.ListBox3)
+        Me.GroupBatocera.Controls.Add(Me.BatoceraTab)
+        Me.GroupBatocera.Controls.Add(Me.Label7)
+        Me.GroupBatocera.Controls.Add(Me.Label8)
+        Me.GroupBatocera.Controls.Add(Me.ListBox4)
+        Me.GroupBatocera.Location = New System.Drawing.Point(428, 132)
+        Me.GroupBatocera.Name = "GroupBatocera"
+        Me.GroupBatocera.Size = New System.Drawing.Size(405, 417)
+        Me.GroupBatocera.TabIndex = 36
+        Me.GroupBatocera.TabStop = False
+        Me.GroupBatocera.Text = "Batocera to Everdrive"
+        '
+        'BatotoGBA
+        '
+        Me.BatotoGBA.Location = New System.Drawing.Point(95, 381)
+        Me.BatotoGBA.Name = "BatotoGBA"
+        Me.BatotoGBA.Size = New System.Drawing.Size(214, 27)
+        Me.BatotoGBA.TabIndex = 25
+        Me.BatotoGBA.Text = "Copy Batocera Saves --> Everdrive Saves"
+        Me.BatotoGBA.UseVisualStyleBackColor = True
         '
         'ListBox3
         '
         Me.ListBox3.FormattingEnabled = True
-        Me.ListBox3.Location = New System.Drawing.Point(619, 343)
+        Me.ListBox3.Location = New System.Drawing.Point(6, 192)
         Me.ListBox3.Name = "ListBox3"
-        Me.ListBox3.Size = New System.Drawing.Size(204, 95)
-        Me.ListBox3.TabIndex = 20
+        Me.ListBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ListBox3.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.ListBox3.Size = New System.Drawing.Size(184, 186)
+        Me.ListBox3.TabIndex = 26
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 171)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 27
+        Me.Label7.Text = "FROM"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(211, 171)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(22, 13)
+        Me.Label8.TabIndex = 29
+        Me.Label8.Text = "TO"
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Location = New System.Drawing.Point(214, 192)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ListBox4.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.ListBox4.Size = New System.Drawing.Size(184, 186)
+        Me.ListBox4.TabIndex = 28
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(898, 496)
-        Me.Controls.Add(Me.ListBox3)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.ListBox2)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Button3)
+        Me.ClientSize = New System.Drawing.Size(834, 553)
+        Me.Controls.Add(Me.GroupBatocera)
+        Me.Controls.Add(Me.GroupGBA)
+        Me.Controls.Add(Me.CalculateDelta)
         Me.Controls.Add(Me.RefreshBatocera)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.BatoceraPath)
-        Me.Controls.Add(Me.BatoceraTab)
-        Me.Controls.Add(Me.EverdriveTab)
-        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox2)
@@ -234,6 +311,10 @@ Partial Class Form1
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.EverdriveTab, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.BatoceraTab, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupGBA.ResumeLayout(False)
+        Me.GroupGBA.PerformLayout()
+        Me.GroupBatocera.ResumeLayout(False)
+        Me.GroupBatocera.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -245,16 +326,22 @@ Partial Class Form1
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
     Friend WithEvents EverdriveTab As DataGridView
     Friend WithEvents BatoceraTab As DataGridView
     Friend WithEvents Label4 As Label
     Friend WithEvents BatoceraPath As TextBox
     Friend WithEvents RefreshBatocera As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents GBAtoBato As Button
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents CalculateDelta As Button
+    Friend WithEvents Label5 As Label
     Friend WithEvents ListBox2 As ListBox
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupGBA As GroupBox
+    Friend WithEvents GroupBatocera As GroupBox
+    Friend WithEvents BatotoGBA As Button
     Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ListBox4 As ListBox
 End Class
--- EverdriveGBAtoBatocera/Form1.vb
+++ EverdriveGBAtoBatocera/Form1.vb
@@ -1,7 +1,26 @@
 ﻿Imports System.IO
 Imports System.Globalization
 Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GBASD.Text = My.Settings.everdriveletter
+        BatoceraPath.Text = My.Settings.batocerauserpath
+        GBAtoBato.Visible = False
+        BatotoGBA.Visible = False
+        CalculateDelta.Visible = False
+        RefreshBatocera.Visible = False
+    End Sub
+    Private Sub GBASD_TextChanged(sender As Object, e As EventArgs) Handles GBASD.TextChanged
+        If GBASD.Text.Length > 1 Then
+            My.Settings.everdriveletter = GBASD.Text
+            My.Settings.Save()
+        End If
+    End Sub
+    Private Sub BatoceraPath_TextChanged(sender As Object, e As EventArgs) Handles BatoceraPath.TextChanged
+        If BatoceraPath.Text.Length > 1 Then
+            My.Settings.batocerauserpath = BatoceraPath.Text
+            My.Settings.Save()
+        End If
+    End Sub
     Private Sub RefreshGBA_Click(sender As Object, e As EventArgs) Handles RefreshGBA.Click
         'Go to SD older 
         Dim gbasavepath As String = Path.Combine(GBASD.Text, "GBASYS", "save")
@@ -87,5 +106,200 @@ nextfile:
 
         'Reajusting Interface and Showing Final Interface
         dv.Sort = "Last Updated desc"
+
+        RefreshBatocera.Visible = True
+    End Sub
+
+    Private Sub RefreshBatocera_Click(sender As Object, e As EventArgs) Handles RefreshBatocera.Click
+        'Go to SD older 
+        Dim batocerasavepath As String = Path.Combine(BatoceraPath.Text, "saves", "gba")
+
+        'Safely Clearing Grid
+        BatoceraTab.Columns.Clear()
+        On Error Resume Next
+        BatoceraTab.Rows.Clear()
+        On Error GoTo 0
+
+        'Creating table
+        Dim table As New DataTable()
+        Dim dv As DataView
+        Dim column As DataColumn
+
+        column = New DataColumn()
+        With column
+            .DataType = Type.GetType("System.String")
+            .ColumnName = "SaveFile"
+        End With
+        table.Columns.Add(column)
+
+        column = New DataColumn()
+        With column
+            .DataType = Type.GetType("System.String")
+            .ColumnName = "Path"
+        End With
+        table.Columns.Add(column)
+
+        column = New DataColumn()
+        With column
+            .DataType = Type.GetType("System.String")
+            .ColumnName = "Last Updated"
+        End With
+        table.Columns.Add(column)
+
+        Dim extension As String
+        Dim savefile As String
+        Dim savepath As String
+        Dim savelastupdated As String
+
+        Dim countfiles As Integer
+
+        Dim di As New IO.DirectoryInfo(batocerasavepath)
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*.srm")
+        Dim fi As IO.FileInfo
+
+        countfiles = aryFi.Length
+
+        For Each fi In aryFi
+            If countfiles > 0 Then
+                'getting infos
+                savefile = fi.Name
+                savepath = fi.FullName
+                Dim lastupdated = fi.LastWriteTime
+
+                savelastupdated = CDate(lastupdated).ToString("yyyyMMdd-HHmmss")
+
+                'adding to table
+                table.Rows.Add(savefile, savepath, savelastupdated)
+            Else
+                'no files !
+                MsgBox("No Files !")
+                Exit Sub
+            End If
+            'next file
+nextfile:
+        Next
+
+        'Sorting A-Z the console
+        dv = table.DefaultView
+        BatoceraTab.DataSource = table
+
+        'Width for columns
+        BatoceraTab.RowHeadersWidth = 25
+        BatoceraTab.Columns("SaveFile").Width = 240
+        BatoceraTab.Columns("Last Updated").Width = 100
+        'Hiding les colonnes
+        BatoceraTab.Columns("Path").Visible = False
+
+        'Reajusting Interface and Showing Final Interface
+        dv.Sort = "Last Updated desc"
+
+        CalculateDelta.Visible = True
+    End Sub
+    Private Sub CalculateDelta_Click(sender As Object, e As EventArgs) Handles CalculateDelta.Click
+        Call CompletingListBoxes()
+        GBAtoBato.Visible = True
+        BatotoGBA.Visible = True
+    End Sub
+    Private Sub CompletingListBoxes()
+        'cleaning listboxes
+        ListBox1.ClearSelected()
+        ListBox2.ClearSelected()
+
+        ListBox3.ClearSelected()
+        ListBox4.ClearSelected()
+
+        Dim destinationpath As String
+
+        For i = 0 To EverdriveTab.Rows.Count - 2
+            Dim savefilegba As String = EverdriveTab.Rows(i).Cells(EverdriveTab.Columns("SaveFile").Index).Value
+            Dim lastupdatedgba As String = Replace(EverdriveTab.Rows(i).Cells(EverdriveTab.Columns("Last Updated").Index).Value, "-", "")
+            Dim pathgba As String = EverdriveTab.Rows(i).Cells(EverdriveTab.Columns("Path").Index).Value
+            Dim filenamesrm As String = Path.GetFileNameWithoutExtension(pathgba) & ".srm"
+
+            'search for it on the other tab
+            destinationpath = Path.Combine(BatoceraPath.Text, "saves", "gba", filenamesrm)
+
+            'adding to listboxes
+            ListBox1.Items.Add(pathgba)
+            ListBox2.Items.Add(destinationpath)
+
+        Next
+
+        For j = 0 To BatoceraTab.Rows.Count - 2
+            Dim savefilebato As String = BatoceraTab.Rows(j).Cells(BatoceraTab.Columns("SaveFile").Index).Value
+            Dim lastupdatedbato As String = Replace(BatoceraTab.Rows(j).Cells(BatoceraTab.Columns("Last Updated").Index).Value, "-", "")
+            Dim pathbato As String = BatoceraTab.Rows(j).Cells(BatoceraTab.Columns("Path").Index).Value
+            Dim savefilename As String = Path.GetFileNameWithoutExtension(savefilebato)
+
+            'search for existing file
+            Dim di As New IO.DirectoryInfo(Path.Combine(GBASD.Text, "GBASYS", "save"))
+            Dim aryFi As IO.FileInfo() = di.GetFiles(savefilename & ".*")
+            Dim fi As IO.FileInfo
+            Dim extension As String
+            Dim findfilepath As String
+
+            Dim countfiles = aryFi.Length
+            If countfiles = 0 Then extension = ".srm"
+
+            For Each fi In aryFi
+                extension = fi.Extension
+            Next
+
+
+            destinationpath = Path.Combine(GBASD.Text, "GBASYS", "save", savefilename & extension)
+
+            'adding to listboxes
+            ListBox3.Items.Add(pathbato)
+            ListBox4.Items.Add(destinationpath)
+        Next
+
+        'Checking if Already add
+
+        For item = 0 To ListBox2.Items.Count - 1
+            If System.IO.File.Exists(ListBox2.Items(item)) Then
+                ListBox2.SetSelected(item, True)
+            End If
+        Next
+
+    End Sub
+
+    Private Sub listBox2_DrawItem(ByVal sender As Object, ByVal e As DrawItemEventArgs)
+        If e.Index < 0 Then Return
+        If (e.State And DrawItemState.Selected) = DrawItemState.Selected Then e = New DrawItemEventArgs(e.Graphics, e.Font, e.Bounds, e.Index, e.State Xor DrawItemState.Selected, e.ForeColor, Color.Red)
+        e.DrawBackground()
+        e.Graphics.DrawString(ListBox2.Items(e.Index).ToString(), e.Font, Brushes.White, e.Bounds, StringFormat.GenericDefault)
+        e.DrawFocusRectangle()
+    End Sub
+
+    Private Sub GBAtoBato_Click(sender As Object, e As EventArgs) Handles GBAtoBato.Click
+        If MsgBox("Are you sure you want to copy all your files from left to right ?", vbYesNo) = vbNo Then Exit Sub
+
+        Dim sourcefile As String
+        Dim destinationfile As String
+
+        For i = 0 To ListBox1.Items.Count
+            sourcefile = ListBox1.Items(i)
+            destinationfile = ListBox2.Items(i)
+            FileCopy(sourcefile, destinationfile)
+        Next
+
+        MsgBox("Finished")
+        Process.Start(Path.Combine(BatoceraPath.Text, "saves", "gba"))
+    End Sub
+
+    Private Sub BatotoGBA_Click(sender As Object, e As EventArgs) Handles BatotoGBA.Click
+        If MsgBox("Are you sure you want to copy all your files from left to right ?", vbYesNo) = vbNo Then Exit Sub
+
+        Dim sourcefile As String
+        Dim destinationfile As String
+
+        For i = 0 To ListBox3.Items.Count
+            sourcefile = ListBox3.Items(i)
+            destinationfile = ListBox4.Items(i)
+            FileCopy(sourcefile, destinationfile)
+        Next
+
+        MsgBox("Finished")
+        Process.Start(Path.Combine(GBASD.Text, "GBASYS", "save"))
     End Sub
 End Class
